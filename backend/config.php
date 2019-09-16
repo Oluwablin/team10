114b212069f88ef9c90d0d0c5c93f5aefd8f5ddb
Oyeleke Oluseun Emmanuel <52815638+Oluwablin@users.noreply.github.com>
2019-09-16 23:02:53 +0100

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -5,7 +5,8 @@ define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'team10');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-if (mysqli_connect_errno()) {
+if (mysqli_connect_error()) {
     // If there is an error with the connection, stop the script and display the error.
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+?>
